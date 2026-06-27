2f4cede7c42b07325a15127ab9ab5dd02c0857da
Moad Hadmen <moad.hadmen-extern@renault.com>
2026-06-27 22:42:31 +0200

    Ignore proxy_off.ps1 (utilitaire local) et retire du suivi

--- proxy_off.ps1
+++
@@ -1,7 +0,0 @@
-Remove-Item Env:HTTP_PROXY -ErrorAction Ignore
-Remove-Item Env:HTTPS_PROXY -ErrorAction Ignore
-
-[Environment]::SetEnvironmentVariable("HTTP_PROXY", $null, "User")
-[Environment]::SetEnvironmentVariable("HTTPS_PROXY", $null, "User")
-
-Write-Output "Proxy desactive"
